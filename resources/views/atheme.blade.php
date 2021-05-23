9bff64777b34c834bcf6b82a08a6b1cb9d3265c1
alvinaji503 <mca503@rajagiri.edu>
2021-05-23 22:30:48 +0530

    m

--- resources/views/atheme.blade.php
+++ resources/views/atheme.blade.php
@@ -22,7 +22,7 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          </div>
+          
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
@@ -54,6 +54,7 @@
           </div>
         </div>
       </nav>
+      </div>
       @yield("content");
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </body>
